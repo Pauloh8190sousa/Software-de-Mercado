5673af53128e12ecd479b8d558fed6fd2c81523e
paulo <paulo@10.0.0.112>
2021-03-30 19:03:35 -0300

    Criação da interface Inicio e atualização de interface

--- Sistema de Mercado/src/app/Aplicacao.java
+++ Sistema de Mercado/src/app/Aplicacao.java
@@ -1,24 +1,26 @@
-package view;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class ViewLogin extends Application{
+public class Aplicacao extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Pane root = FXMLLoader.load(getClass().getResource("ViewLogin.fxml"));
+		Pane root = FXMLLoader.load(getClass().getResource("/view/ViewLogin.fxml"));
 		Scene scene = new Scene(root, 600, 400);
 		arg0.setScene(scene);
 		arg0.setTitle("Mercado");
+		arg0.getIcons().add(new Image("/icons8-full-shopping-basket-30.png"));
 		arg0.show();
-		
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	
 }
--- Sistema de Mercado/src/controller/ControllerViewBase.java
+++ Sistema de Mercado/src/controller/ControllerViewBase.java
@@ -1,16 +1,24 @@
-package view;
+package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class ControllerViewBase implements Initializable{
 	@FXML
@@ -23,7 +31,7 @@ public class ControllerViewBase implements Initializable{
 	private AnchorPane anchorPane;
 	
 	@FXML
-	private void acaoButton(ActionEvent event){
+	private void acaoButton(ActionEvent event) throws Exception{
 		if(campoEmail.getText().equals("")&&campoSenha.getText().equals("")){
 			JOptionPane.showMessageDialog(null, "Campo E-mail e senha vazios");
 			
@@ -34,7 +42,16 @@ public class ControllerViewBase implements Initializable{
 			JOptionPane.showMessageDialog(null, "Campo Senha Vazio");
 		}
 		else{
-			//em edição
+			Stage s1 = new Stage();
+			 Pane root = FXMLLoader.load(getClass().getResource("/view/ViewInicio.fxml"));
+			 Scene scene = new Scene(root,600,400);
+			 
+			 Stage stage = (Stage) anchorPane.getScene().getWindow();
+			 stage.close();
+			 s1.setScene(scene);
+			 s1.setTitle("Mercado");
+			 s1.getIcons().add(new Image("/icons8-full-shopping-basket-30.png"));
+			 s1.show();
 		}
 		
 	}
