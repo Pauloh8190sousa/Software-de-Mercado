a38d13f1738c963bbdffa360ce7cae461f5387b1
paulo <paulo@10.0.0.112>
2021-04-04 12:52:12 -0300

    Criação de sistema de Update para futuras atualizações e criação de arquivo para controle de versão

--- Sistema de Mercado/src/controller/ControllerInicio.java
+++ Sistema de Mercado/src/controller/ControllerInicio.java
@@ -2,11 +2,17 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.layout.GridPane;
 
 public class ControllerInicio implements Initializable{
-
+	@FXML
+	private Group grupo;
+	@FXML
+	private GridPane grid;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
---
+++ Sistema de Mercado/src/system/Update.java
@@ -0,0 +1,68 @@
+package system;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Update {
+
+
+    /** Arquivo para update */
+    private String urlUpdateFile = "http://ENDEREÇO_DO_SEU_JAR/arquivo.jar";
+
+    /** Faz download de arquivo */
+    public void download(String address, String localFileName, String host, int porta) {
+
+            // leitor do arquivo a ser baixado
+            InputStream in = null;
+            // conexão com a internete
+            URLConnection conn = null;
+            // escritor do arquivo que será baixado
+            OutputStream out = null;
+
+            System.out.println("Update.download() BAIXANDO " + address);
+
+            try {
+                    URL url = new URL(address);
+                    out = new BufferedOutputStream(new FileOutputStream(localFileName));
+
+                    // verifica se existe proxy
+                    if(host!=""&&host!=null){
+                            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host,porta));
+                            conn = url.openConnection(proxy);
+                    }else{
+                            conn = url.openConnection();
+                    }
+
+                    in = conn.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int numRead;
+                    long numWritten = 0;
+                    while ((numRead = in.read(buffer)) != -1) {
+                            out.write(buffer, 0, numRead);
+                            numWritten += numRead;
+                    }
+                    System.out.println(localFileName + "\t" + numWritten);
+
+            } catch (Exception exception) {
+                    exception.printStackTrace();
+            } finally {
+                    try {
+                            if (in != null) {
+                                    in.close();
+                            }
+                            if (out != null) {
+                                    out.close();
+                            }
+                    } catch (IOException ioe) {
+                    }
+            }
+    }
+
+}
