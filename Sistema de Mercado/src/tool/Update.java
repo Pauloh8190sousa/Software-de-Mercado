fa7e355ddb1f459b3e760b51d77e06e1502ff472
paulo <paulo@10.0.0.116>
2021-04-07 10:10:04 -0300

    Correções de erros.

--- Sistema de Mercado/src/tool/Update.java
+++ Sistema de Mercado/src/tool/Update.java
@@ -12,57 +12,56 @@ import java.net.URLConnection;
 
 public class Update {
 
+	/** Arquivo para update */
+	private String urlUpdateFile = "http://ENDEREÇO_DO_SEU_JAR/arquivo.jar";
 
-    /** Arquivo para update */
-    private String urlUpdateFile = "http://ENDEREÇO_DO_SEU_JAR/arquivo.jar";
+	/** Faz download de arquivo */
+	public void download(String address, String localFileName, String host, int porta) {
 
-    /** Faz download de arquivo */
-    public void download(String address, String localFileName, String host, int porta) {
+		// leitor do arquivo a ser baixado
+		InputStream in = null;
+		// conexão com a internete
+		URLConnection conn = null;
+		// escritor do arquivo que será baixado
+		OutputStream out = null;
 
-            // leitor do arquivo a ser baixado
-            InputStream in = null;
-            // conexão com a internete
-            URLConnection conn = null;
-            // escritor do arquivo que será baixado
-            OutputStream out = null;
+		System.out.println("Update.download() BAIXANDO Atualização " + address);
 
-            System.out.println("Update.download() BAIXANDO " + address);
+		try {
+			URL url = new URL(address);
+			out = new BufferedOutputStream(new FileOutputStream(localFileName));
 
-            try {
-                    URL url = new URL(address);
-                    out = new BufferedOutputStream(new FileOutputStream(localFileName));
+			// verifica se existe proxy
+			if (host != "" && host != null) {
+				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, porta));
+				conn = url.openConnection(proxy);
+			} else {
+				conn = url.openConnection();
+			}
 
-                    // verifica se existe proxy
-                    if(host!=""&&host!=null){
-                            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host,porta));
-                            conn = url.openConnection(proxy);
-                    }else{
-                            conn = url.openConnection();
-                    }
+			in = conn.getInputStream();
+			byte[] buffer = new byte[1024];
+			int numRead;
+			long numWritten = 0;
+			while ((numRead = in.read(buffer)) != -1) {
+				out.write(buffer, 0, numRead);
+				numWritten += numRead;
+			}
+			System.out.println(localFileName + "\t" + numWritten);
 
-                    in = conn.getInputStream();
-                    byte[] buffer = new byte[1024];
-                    int numRead;
-                    long numWritten = 0;
-                    while ((numRead = in.read(buffer)) != -1) {
-                            out.write(buffer, 0, numRead);
-                            numWritten += numRead;
-                    }
-                    System.out.println(localFileName + "\t" + numWritten);
-
-            } catch (Exception exception) {
-                    exception.printStackTrace();
-            } finally {
-                    try {
-                            if (in != null) {
-                                    in.close();
-                            }
-                            if (out != null) {
-                                    out.close();
-                            }
-                    } catch (IOException ioe) {
-                    }
-            }
-    }
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		} finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+				if (out != null) {
+					out.close();
+				}
+			} catch (IOException ioe) {
+			}
+		}
+	}
 
 }
