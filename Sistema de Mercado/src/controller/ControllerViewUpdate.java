f7d586a2e6403ee41177486351a00a06fbc0862b
paulo <paulo@LAPTOP-AA5HVU43>
2021-04-14 14:56:02 -0300

    Atualização da tela de Update e desenvolvimento do Update de versão

--- Sistema de Mercado/src/controller/ControllerViewUpdate.java
+++ Sistema de Mercado/src/controller/ControllerViewUpdate.java
@@ -1,12 +1,17 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
+
+import javax.swing.filechooser.FileSystemView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressIndicator;
+import tool.Update;
 
 public class ControllerViewUpdate implements Initializable{
 	@FXML
@@ -14,10 +19,27 @@ public class ControllerViewUpdate implements Initializable{
 	
 	@FXML
 	private void procurarUpdate(ActionEvent event){
-		progress.setVisible(true);
-		
+//		if(testarConexao()==false){
+//			JOptionPane.showMessageDialog(null, "Sem conexão com a internet");
+//		}else{
+			progress.setVisible(true);
+			Update up = new Update();
+			FileSystemView system = FileSystemView.getFileSystemView();
+			String path = system.getHomeDirectory().getPath();
+			System.out.println(path+"/teto.mp3");
+			up.dowload(path+"/notificacao.mp3");
+				System.out.println("Executado");
+//		}
 	}
-	
+   private static boolean testarConexao() {
+     try {
+       URLConnection connection = (new URL("http://www.google.com.br")).openConnection();
+       connection.connect();
+       return true;
+     } catch (IOException e) {
+       return false;
+     } 
+   }
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
--- Sistema de Mercado/src/tool/Update.java
+++ Sistema de Mercado/src/tool/Update.java
@@ -9,59 +9,83 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.auth.DbxAppAuthRequests;
+import com.dropbox.core.v2.files.DbxAppFilesRequests;
+import com.dropbox.core.v2.users.FullAccount;
+
 
 public class Update {
 
 	/** Arquivo para update */
-	private String urlUpdateFile = "http://ENDEREÇO_DO_SEU_JAR/arquivo.jar";
+//	private String urlUpdateFile = "http://ENDEREÇO_DO_SEU_JAR/arquivo.jar";
 
 	/** Faz download de arquivo */
-	public void download(String address, String localFileName, String host, int porta) {
+	public void atualizar(String address, String localFileName, String host, int porta) {
 
-		// leitor do arquivo a ser baixado
-		InputStream in = null;
-		// conexão com a internete
-		URLConnection conn = null;
-		// escritor do arquivo que será baixado
-		OutputStream out = null;
-
-		System.out.println("Update.download() BAIXANDO Atualização " + address);
-
-		try {
-			URL url = new URL(address);
-			out = new BufferedOutputStream(new FileOutputStream(localFileName));
-
-			// verifica se existe proxy
-			if (host != "" && host != null) {
-				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, porta));
-				conn = url.openConnection(proxy);
-			} else {
-				conn = url.openConnection();
-			}
-
-			in = conn.getInputStream();
-			byte[] buffer = new byte[1024];
-			int numRead;
-			long numWritten = 0;
-			while ((numRead = in.read(buffer)) != -1) {
-				out.write(buffer, 0, numRead);
-				numWritten += numRead;
-			}
-			System.out.println(localFileName + "\t" + numWritten);
-
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-				if (out != null) {
-					out.close();
-				}
-			} catch (IOException ioe) {
-			}
-		}
+//		// leitor do arquivo a ser baixado
+//		InputStream in = null;
+//		// conexão com a internete
+//		URLConnection conn = null;
+//		// escritor do arquivo que será baixado
+//		OutputStream out = null;
+//
+//		System.out.println("Update.download() BAIXANDO Atualização " + address);
+//
+//		try {
+//			URL url = new URL(address);
+//			out = new BufferedOutputStream(new FileOutputStream(localFileName));
+//
+//			// verifica se existe proxy
+//			if (host != "" && host != null) {
+//				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, porta));
+//				conn = url.openConnection(proxy);
+//			} else {
+//				conn = url.openConnection();
+//			}
+//
+//			in = conn.getInputStream();
+//			byte[] buffer = new byte[1024];
+//			int numRead;
+//			long numWritten = 0;
+//			while ((numRead = in.read(buffer)) != -1) {
+//				out.write(buffer, 0, numRead);
+//				numWritten += numRead;
+//			}
+//			System.out.println(localFileName + "\t" + numWritten);
+//
+//		} catch (Exception exception) {
+//			exception.printStackTrace();
+//		} finally {
+//			try {
+//				if (in != null) {
+//					in.close();
+//				}
+//				if (out != null) {
+//					out.close();
+//				}
+//			} catch (IOException ioe) {
+//			}
+//		}
 	}
-
+	public void dowload(String filename){
+//		String url="https://www.dropbox.com/preview/Mercado%20Tech/iPhone%20X%20Note%20(Notifica%C3%A7%C3%A3o).mp3"; 
+//		try{ 
+//		URL download=new URL(url); 
+//		ReadableByteChannel rbc=Channels.newChannel(download.openStream()); 
+//		FileOutputStream fileOut = new FileOutputStream(filename); 
+//		fileOut.getChannel().transferFrom(rbc, 0, 1 << 24); 
+//		fileOut.flush(); 
+//		fileOut.close(); 
+//		rbc.close();
+//	        
+//		System.out.println("Baixou");
+//		}catch(Exception e){ 
+//			e.printStackTrace();
+//			} 
+	}
 }
