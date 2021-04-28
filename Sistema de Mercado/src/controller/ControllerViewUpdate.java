033c305debd8f85423bb62fa1e6bf938b289e5ca
paulo <paulo@LAPTOP-AA5HVU43>
2021-04-28 15:44:14 -0300

    Update completo, e construção da view de cadastrar produtos. Andamento da view listar produtos. Adicionado notificações com a API TryTester.

--- Sistema de Mercado/src/controller/ControllerViewUpdate.java
+++ Sistema de Mercado/src/controller/ControllerViewUpdate.java
@@ -4,49 +4,114 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ResourceBundle;
-
-import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileSystemView;
-
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
+import javafx.util.Duration;
 import tool.Update;
+import tray.animations.AnimationType;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
 
-public class ControllerViewUpdate implements Initializable{
+public class ControllerViewUpdate implements Initializable {
 	@FXML
-	private ProgressIndicator progress;
-	
+	private ProgressBar progress;
 	@FXML
-	private void procurarUpdate(ActionEvent event){
-		if(testarConexao()==false){
-			JOptionPane.showMessageDialog(null, "Sem conexão com a internet");
-		}else{
-			progress.setVisible(true);
+	private ProgressIndicator indicador;
+	@FXML
+	private Label texto;
+	private float megabytes;
+
+	@FXML
+	private void procurarUpdate(ActionEvent event) {
+		if (testarConexao() == false) {
+			TrayNotification tray = new TrayNotification();
+			AnimationType type = AnimationType.POPUP;
+			tray.setAnimationType(type);
+			tray.setTitle("Sem acesso a internet");
+			tray.setMessage("Verifique sua internet");
+			tray.setNotificationType(NotificationType.ERROR);
+			tray.showAndDismiss(Duration.millis(2000));
+		} else {
+			indicador.setVisible(true);
+			texto.setVisible(true);
+			texto.setText("Baixando arquivos recentes");
 			FileSystemView system = FileSystemView.getFileSystemView();
 			String path = system.getHomeDirectory().getPath();
-			new Thread(){
-				@Override
+			String url = "URL";
+			Thread t1 = new Thread() {
 				public void run() {
-					Update.atualizar("URL DO ARQUIVO", path);
-				}
-			}.start();
+					megabytes = Update.gravaArquivoDeURL(url, path, "deu certo", ".mp4", progress, indicador);
+					if (megabytes > 0) {
+						Thread thread = new Thread(new Task() {
+
+							@Override
+							protected Object call() throws Exception {
+
+								Platform.runLater(() -> {
+									TrayNotification tray = new TrayNotification();
+									AnimationType type = AnimationType.POPUP;
+									tray.setAnimationType(type);
+									tray.setTitle("Atualização concluida");
+									tray.setMessage("Arquivo baixado");
+									tray.setNotificationType(NotificationType.SUCCESS);
+									tray.showAndDismiss(Duration.millis(2000));
+									texto.setText("Tamanho da atualização: " + megabytes + "MB");
+								});
+								return null;
+							}
+						});
+						thread.start();
+						thread.interrupt();
+					}else{
+						Thread thread = new Thread(new Task() {
+
+							@Override
+							protected Object call() throws Exception {
+
+								Platform.runLater(() -> {
+									TrayNotification tray = new TrayNotification();
+									AnimationType type = AnimationType.POPUP;
+									tray.setAnimationType(type);
+									tray.setTitle("Sistema na última versão");
+									tray.setMessage("O sistema se encontra na última versão");
+									tray.setNotificationType(NotificationType.SUCCESS);
+									tray.showAndDismiss(Duration.millis(2000));
+									texto.setText("Nenhum atualização disponivel no momento");
+								});
+								return null;
+							}
+						});
+						thread.start();
+						thread.interrupt();
+					}
+				};
+			};
+			t1.start();
+			t1.interrupt();
 		}
 	}
-   private static boolean testarConexao() {
-     try {
-       URLConnection connection = (new URL("http://www.google.com.br")).openConnection();
-       connection.connect();
-       return true;
-     } catch (IOException e) {
-       return false;
-     } 
-   }
+
+	private static boolean testarConexao() {
+		try {
+			URLConnection connection = (new URL("http://www.google.com.br")).openConnection();
+			connection.connect();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- Sistema de Mercado/src/tool/Update.java
+++ Sistema de Mercado/src/tool/Update.java
@@ -1,43 +1,50 @@
 package tool;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 
 public class Update {
 	
-	public static File atualizar(String stringUrl, String pathLocal){
+	public synchronized static float gravaArquivoDeURL(String archive, String local, String nomeDoArquivo, String extesao, ProgressBar progress, ProgressIndicator indicador) {
+		double bytesFile;
 		try {
-//			Desktop desk = Desktop.getDesktop();
-//			desk.browse(new URI(stringUrl));
+			URL url = new URL(archive);
+
+			URLConnection con = url.openConnection();
+
+			BufferedInputStream buf = new BufferedInputStream(con.getInputStream());
+
+			FileOutputStream fos = new FileOutputStream(local + "/" + nomeDoArquivo + extesao);
 			
-			URL url = new URL(stringUrl);
-			
-			String nomeArquivoLocal = "version";
-
-            URLConnection con = url.openConnection();
-            
-            BufferedInputStream buf = new BufferedInputStream(con.getInputStream());
-            
-            FileOutputStream fos = new FileOutputStream(pathLocal+nomeArquivoLocal+".json");
-
-	            int i = 0;
-	            byte[] bytesIn = new byte[100*1000*1024];
-	            while ((i = buf.read(bytesIn)) >= 0) {
-	                fos.write(bytesIn, 0, i);
-	            }
-
+			bytesFile = ((con.getContentLengthLong() / 1024) / 1024);
+			int totalBytesCopied = 0;
+			int i = 0;
+			byte[] bytesIn = new byte[1024];
+			indicador.setVisible(false);
+			progress.setVisible(true);
+			while ((i = buf.read(bytesIn)) >= 0) {
+				fos.write(bytesIn, 0, i);
+				totalBytesCopied += i;
+				System.out.println("totalCopiado: " + totalBytesCopied);
+				if (bytesFile != -1) {
+					double megabytes = ((totalBytesCopied / 1024) / 1024);
+					float percentual = (float) ((megabytes * 100) / bytesFile);
+					progress.setProgress(percentual/100);
+				} else {
+					System.out.println("???");
+					System.out.println("???%");
+				}
+			}
 			fos.close();
 			buf.close();
-
-			return new File(pathLocal+nomeArquivoLocal+".json");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception ex) {
+			return 0;
 		}
-		return null;
+		return (float) bytesFile;
 	}
 
 }
