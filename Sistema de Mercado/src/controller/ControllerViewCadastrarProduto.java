30f437b065dc49f608ebda6b89c0f6e3b32edb15
paulo <paulo@LAPTOP-AA5HVU43>
2021-04-30 16:09:26 -0300

    Correção de bugs e view de casdatro de produto finalizado

--- Sistema de Mercado/src/controller/ControllerViewCadastrarProduto.java
+++ Sistema de Mercado/src/controller/ControllerViewCadastrarProduto.java
@@ -1,15 +1,17 @@
 package controller;
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 import javafx.util.Duration;
-import tool.CampoFormatadoText;
+import system.Caixa;
 import tray.animations.AnimationType;
 import tray.notification.NotificationType;
 import tray.notification.TrayNotification;
@@ -25,14 +27,17 @@ public class ControllerViewCadastrarProduto implements Initializable{
 	private DatePicker dateFabricacao;
 	
 	@FXML
+	private void fomatarText(){
+		
+	}
+	@FXML
 	public void cadastrarProduto(ActionEvent event){
 		String nome = campoNomeProduto.getText();
 		float preco = Float.parseFloat(campoPreco.getText());
-		System.out.println(preco);
 		LocalDate dateVali = dateValidade.getValue();
 		LocalDate dateFabri = dateFabricacao.getValue();
-//		Caixa caixa = Caixa.getInstance();
-//		caixa.addProductEstoque(preco, 0, nome, 0,dateVali, dateFabri);
+		Caixa caixa = Caixa.getInstance();
+		caixa.addProductEstoque(preco, 0, nome, 0,dateVali, dateFabri);
 		TrayNotification tray = new TrayNotification();
 		AnimationType type = AnimationType.POPUP;
 		tray.setAnimationType(type);
@@ -40,20 +45,23 @@ public class ControllerViewCadastrarProduto implements Initializable{
 		tray.setMessage("Cadastrado o produto com sucesso");
 		tray.setNotificationType(NotificationType.SUCCESS);
 		tray.showAndDismiss(Duration.millis(2000));
+		campoNomeProduto.setText("");
+		campoPreco.setText("");
+		
 	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-//		UnaryOperator<Change> filter = change ->{
-//			String text = change.getText();
-//			if(!text.matches("") && text.length()<5){
-//				return change;
-//			}
-//			return null;
-//		};
-//		TextFormatter<String> textFormater = new TextFormatter<>(filter);
-		CampoFormatadoText campoForm = new CampoFormatadoText(new Locale("pt", "BR"), campoPreco);
-		campoPreco = campoForm.getCampo();
+		UnaryOperator<Change> filter = change ->{
+			String text = change.getControlNewText();
+			if(text.matches("^[/[0-9].?!]*") && text.length()<=5){
+				return change;
+			}
+			return null;
+		};
+		TextFormatter<String> textFormater = new TextFormatter<>(filter);
+		
+		campoPreco.setTextFormatter(textFormater);
 		
 	}
 
