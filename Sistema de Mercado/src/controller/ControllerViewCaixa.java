91045bef5bb51775aa5396971bf91068df7f04f3
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-05-13 10:10:34 -0300

    Adição da lógica do caixa para somar os preços de produtos. Adicição de notificações de erros e somatorio para o sistema.

--- Sistema de Mercado/src/controller/ControllerViewCaixa.java
+++ Sistema de Mercado/src/controller/ControllerViewCaixa.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -17,34 +18,76 @@ import tray.animations.AnimationType;
 import tray.notification.NotificationType;
 import tray.notification.TrayNotification;
 
-public class ControllerViewCaixa implements Initializable{
+public class ControllerViewCaixa implements Initializable {
 	@FXML
 	private GridPane grid;
 	@FXML
 	private TextArea textArea;
-	
+
 	@FXML
-	private void voltar(ActionEvent event) throws IOException{
+	private void teclado(KeyEvent keyEvent) {
+		if (keyEvent.getText().matches("[0-9]")) {
+			if (textArea.getText().equals("")) {
+				textArea.setText(keyEvent.getText());
+			} else {
+				textArea.setText(textArea.getText() + "\n" + keyEvent.getText());
+			}
+		}
+	}
+
+	@FXML
+	private void voltar(ActionEvent event) throws IOException {
 		Stage stage = (Stage) grid.getScene().getWindow();
 		Pane root = FXMLLoader.load(getClass().getResource("/view/ViewInicio.fxml"));
 		Scene scene = new Scene(root, stage.getScene().getWidth(), stage.getScene().getHeight());
 		stage.setScene(scene);
 	}
+
 	@FXML
-	private void somar(ActionEvent event){
-		textArea.setText("R$ 00,00");
+	private void somar(ActionEvent event) {
 		TrayNotification tray = new TrayNotification();
 		AnimationType type = AnimationType.POPUP;
 		tray.setAnimationType(type);
 		tray.setTitle("SOMA TOTAL!");
-		tray.setMessage("SOMA: "+textArea.getText());
-		tray.setNotificationType(NotificationType.INFORMATION);
-		tray.showAndDismiss(Duration.millis(2000));
+
+		if (textArea.getText().contains("-")) {
+			
+			tray.setTitle("ATENÇÃO!");
+			tray.setMessage("SOMA TOTAL JÁ FOI EXECUTADA");
+			tray.setNotificationType(NotificationType.WARNING);
+			tray.showAndDismiss(Duration.millis(3000));
+			
+		}else if(textArea.getText().equals("")){
+			tray.setTitle("NENHUM NÚMERO FOI INSERIDO");
+			tray.setMessage("NÃO HÁ NÚMEROS\nDIGITE ALGUM NÚMERO PARA SOMAR!");
+			tray.setNotificationType(NotificationType.ERROR);
+			tray.showAndDismiss(Duration.millis(3000));
+		}else {
+			
+			String[] array = textArea.getText().split("\n");
+			float soma = 0;
+			for (int i = 0; i < array.length; i++) {
+				float num = Float.parseFloat(array[i]);
+				soma += num;
+			}
+			textArea.setText(textArea.getText() + "\n----------------\n");
+			textArea.setText(textArea.getText() + "Total: R$ " + soma);
+			tray.setMessage("SOMA: " + soma);
+			tray.setNotificationType(NotificationType.INFORMATION);
+			tray.showAndDismiss(Duration.millis(2000));
+		}
+
 	}
+
+	@FXML
+	private void limpar(ActionEvent event) {
+		textArea.setText("");
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
